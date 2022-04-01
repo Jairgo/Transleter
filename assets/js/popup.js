88c424865aa01eb5ba3524ca2e5ece84f190e693
Jair Gómez Vásquez <jair.gomezv06@anahuac.mx>
2022-04-01 01:20:30 -0600

    ADD Listas de idiomas origen y destino

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,9 +1,57 @@
 const text = document.getElementById('translate-text');
 const translate = document.getElementById('translate-button');
+const reset = document.getElementById('translate-reset');
+var from = document.getElementsByName('fromLanguages')[0];
+var to = document.getElementsByName('toLanguages')[0];
+var fromValue = document.getElementById("fromId");
+var toValue = document.getElementById("toId");
 
+// Obtener los valores de los idiomas almacenados en el globalStorage
+chrome.storage.sync.get('from', function (result) {
+    fromValue.value = result.from;
+});
+chrome.storage.sync.get('to', function (result) {
+    toValue.value = result.to;
+});
+
+// Obtener los valores de los idiomas a traducir
+from.addEventListener('input', function () {
+    from = this.value;
+});
+
+to.addEventListener('input', function () {
+    to = this.value;
+});
+
+// Listener para el boton de traducir
 translate.addEventListener('click', () => {
+
+    // Almacenar los valores de los idiomas en el globalStorage
+    chrome.storage.sync.set({
+        from: from,
+        to: to
+    });
+
+    // Enviar el idioma origen al background.js
     chrome.runtime.sendMessage('', {
-        type: 'notification',
+        type: 'from',
+        message: from
+    });
+
+    // Enviar el idioma destino al background.js
+    chrome.runtime.sendMessage('', {
+        type: 'to',
+        message: to
+    });
+
+    // Enviar el texto a traducir al background.js
+    chrome.runtime.sendMessage('', {
+        type: 'translate',
         message: text.value
     });
+});
+
+// Listener para el boton de resetear
+reset.addEventListener('click', () => {
+    text.value = '';
 });
--- background.js
+++ background.js
@@ -1,18 +1,32 @@
+// Parametros de la peticion
 var key = "5cba5fc42bed40b7aa31630170fcb70f";
 var endpoint = "https://api.cognitive.microsofttranslator.com/";
 var location = "southcentralus";
 
+// Headers para la peticion
 var myHeaders = new Headers();
 myHeaders.append("Ocp-Apim-Subscription-Key", key);
 myHeaders.append("Ocp-Apim-Subscription-Region", location);
 myHeaders.append("Content-type", "application/json");
-var from = "es";
-var to = "en";
 
+var from = "";
+var to = "";
+
+// Listener para recibir el texto a traducir, el idioma origen y destino desde el popup
 chrome.runtime.onMessage.addListener(data => {
-    if (data.type === 'notification') {
+
+    if (data.type === 'from') {
+        from = data.message;
+    }
+
+    if (data.type === 'to') {
+        to = data.message;
+    }
+
+    if (data.type === 'translate') {
         getTranslation(data.message, from, to);
     }
+
 });
 
 chrome.runtime.onInstalled.addListener(() => {
@@ -21,14 +35,21 @@ chrome.runtime.onInstalled.addListener(() => {
         title: "Traducir: %s",
         contexts: ["selection"]
     });
+
+    chrome.storage.sync.set({
+        from: from,
+        to: to
+    });
 });
 
+// Listener para recibir el texto a traducir, el idioma origen y destino desde el click derecho
 chrome.contextMenus.onClicked.addListener((info, tab) => {
     if ('translate' === info.menuItemId) {
         getTranslation(info.selectionText, from, to);
     }
 });
 
+// Funcion para obtener la traduccion
 const getTranslation = (text, from, to) => {
 
     var raw = JSON.stringify([
@@ -52,7 +73,7 @@ const getTranslation = (text, from, to) => {
 
 };
 
-
+// Funcion para mostrar la traduccion como notificacion
 const showTranslate = message => {
     return chrome.notifications.create(
         '',
